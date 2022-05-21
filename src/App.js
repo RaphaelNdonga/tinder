7908514042e850d387225b09eb1fe071c236db6d
RaphaelNdonga <nkibi53@gmail.com>
2022-05-21 15:49:12 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    //bem class naming convention
+    <div className="app">
+      <h1>Lets build the mern tinder clone guys</h1>
+      {/** Header */}
+      {/** Tinder Cards */}
+      {/** Swipe buttons */}
     </div>
   );
 }
